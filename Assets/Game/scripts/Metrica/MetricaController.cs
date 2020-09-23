4652fa08d6db4998c29ebbb8c00b0a667e471010
nedved8891 <nedved8891@gmail.com>
2020-09-23 11:53:55 +0300

    Добавив метод з івентами які варто викликати

--- Assets/Game/scripts/Metrica/MetricaController.cs
+++ Assets/Game/scripts/Metrica/MetricaController.cs
@@ -82,4 +82,76 @@ public class MetricaController : MonoBehaviour
     {
         metrica.SendEventsBuffer();
     }
+
+    /// <summary>
+    /// событие отказа продолжать за просмотр рекламы (у тех у кого реклама еще есть, а у тех у кого нет просто отказ продолжить играть после первой неудачной попытки)
+    /// </summary>
+    public void ContinueAdsCancel()
+    {
+        eventValue = "continue_ads_cancel";
+
+        AppMetrica.Instance.ReportEvent(eventValue);
+
+        SendBuffer();
+    }
+
+    /// <summary>
+    /// сворачивание приложения во время просмотра рекламы для продолжения игры
+    /// </summary>
+    public void ContinueAdsExit()
+    {
+        eventValue = "continue_ads_exit";
+
+        AppMetrica.Instance.ReportEvent(eventValue);
+
+        SendBuffer();
+    }
+
+    /// <summary>
+    /// попытка купить отсутствие рекламы
+    /// </summary>
+    public void TryingToBuyNoADs()
+    {
+        eventValue = "TryingToBuyNoADs";
+
+        AppMetrica.Instance.ReportEvent(eventValue);
+
+        SendBuffer();
+    }
+
+    /// <summary>
+    /// успешно посмотрел рекламу при старте и начал играть
+    /// </summary>
+    public void StartAdsViewSuccess()
+    {
+        eventValue = "start_ads_view_ success";
+
+        AppMetrica.Instance.ReportEvent(eventValue);
+
+        SendBuffer();
+    }
+
+    /// <summary>
+    /// сворачивание приложения/выход на рекламе при старте
+    /// </summary>
+    public void StartAdsFail()
+    {
+        eventValue = "start_ads_fail";
+
+        AppMetrica.Instance.ReportEvent(eventValue);
+
+        SendBuffer();
+    }
+
+    /// <summary>
+    /// успешно посмотрел рекламу за продолжение игры
+    /// </summary>
+    public void ContinueAdsViewComplete()
+    {
+        eventValue = "continue_ads_view_complete";
+
+        AppMetrica.Instance.ReportEvent(eventValue);
+
+        SendBuffer();
+    }
 }
