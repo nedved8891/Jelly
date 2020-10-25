9118461f23b5b95e467593fd48537661dea5b06c
nedved <nedved8891@gmail.com>
2020-10-25 12:30:07 +0200

    Добавив, щоб банер показувався через 90 сек і на 15 сек

---
+++ Assets/Game/scripts/TimeVisibleSmallBanner.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using AdColony;
+using BSH_Prototype;
+using UnityEngine;
+
+public class TimeVisibleSmallBanner : MonoBehaviour
+{
+    public float lastTime;
+
+    public float delayShow = 120;
+
+    public float deleyHide = 10;
+
+    public bool visible;
+
+    private void Start()
+    {
+        lastTime = -delayShow;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!visible)
+        {
+            if (Time.time - lastTime > delayShow)
+            {
+                if(SceneLoader.Instance)
+                    SceneLoader.Instance.CheckSmallBanner(SceneLoader.Instance.GetCurrentScene());
+
+                lastTime = Time.time;
+
+                visible = true;
+            }
+        }else
+        {
+            if (Time.time - lastTime > deleyHide)
+            {
+                if(AdsProvider.Instance)
+                    AdsProvider.Instance.HideBannerAd();
+
+                lastTime = Time.time;
+                
+                visible = false;
+            }
+        }
+    }
+}
